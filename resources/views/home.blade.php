368d28c671cfec99fabf59c550e5d59dcd7310dd
Agonpas <agonpas@gmail.com>
2024-02-11 14:28:28 +0100

    Layout of the index page done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
 
-<main>
+<main class="mx-20 my-4">
     
-    <div class="bg-green-500 text-center mx-20 mt-4 mb-10 px-2 py-2 space-y-2">
+    <div class="bg-green-500 text-center px-2 py-2 space-y-2">
         <div class="bg-white text-center mx-1 my-1 px-1 py-1 space-y-auto space-x-auto justify-between">
             <div class="my-1 mx-1 px-3 py-2 bg-green-500">
                 <h1 class="text-center text-white px-2 mt-auto text-2xl font-bold">BIENVENIDOS A LA GESTIÓN DE LA LIGA</h1>
--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -39,7 +39,7 @@
     
     @yield('content')
 
-    <footer class="fixed inset-x-0 bottom-0 bg-green-500 text-center mx-20 my-3 px-2 py-2 space-y-2">
+    <footer class=" inset-x-0 bottom-0 bg-green-500 text-center mx-20 my-3 px-2 py-2 space-y-2">
 		<div class="bg-white text-center mx-1 my-1 px-1 py-1 space-y-auto space-x-auto justify-between">
             <div class="my-1 mx-1 px-3 py-2 bg-green-500">
                 <p class="text-center text-white px-2 mt-auto">creado por Albert</p>
